a9f73e7f8e971faf4d377b45bb8b108727023f7b
25Js <js3773882@gmail.com>
2025-10-14 11:03:32 +0530

    dom

---
+++ javascript/dom.js
@@ -0,0 +1,6 @@
+const d=document.createElement("div");
+const h1=document.createElement("h1");
+h1.innerText="ABES COLLEGE GHAZIABAD";
+d.appendChild(h1);
+const i=document.getElementById("root");
+i.appendChild(d);
